aada5f7d9d80ff7481f7bba880b858cc8c9dffe4
Rutika Banakar <rutikasb@gmail.com>
2019-08-01 18:29:05 -0700

    Fixes

--- face_recognition_tx2.py
+++ face_recognition_tx2.py
@@ -6,17 +6,11 @@ import numpy as np
 import urllib
 from PIL import Image
 import tensorflow.contrib.tensorrt as trt
+import glob
 import face_recognition
-# import matplotlib.pyplot as pyplot
-# import matplotlib.patches as patches
-# import tensorflow as tf
-# from scipy.spatial.distance import cosine
-# from mtcnn.mtcnn import MTCNN
-# from keras_vggface.vggface import VGGFace
-# from keras_vggface.utils import preprocess_input
 import send_email_attach_aws_ses as sendemail
 
-# Our list of known face encodings and a matching list of metadata about each face.
+# Our list of known face encodings
 known_face_encodings = []
 
 def load_known_faces():
@@ -44,7 +38,15 @@ def lookup_known_face(face_encoding):
 
     return face_distances[best_match_index]
 
+def save_image(frame):
+    timestamp = int(time.time())
+    impath = "images-visitors/face-" + str(timestamp) + ".jpg"
+
+    cv2.imwrite(impath, frame)
+    return impath
+
 def check_if_matched(score, threshold=0.5):
+    print(f'Best match score {score}')
     if score <= threshold:
         return True
     else:
@@ -64,30 +66,28 @@ while(True):
     time.sleep(2)
     if ret == True:
         print("captured image")
+        gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # face detection
         faces = face_cascade.detectMultiScale(gray_img, 1.3, 5)
         for (x, y, w, h) in faces:
             t0 = time.time()
-            # cur_img_path = get_wm_image(frame)
-            # print(cur_img_path)
-            # unknown_embed = get_embeddings([cur_img_path])
-            # isMatched = is_match(rm_embed, unknown_embed)
 
-            # print_match(isMatched)
+            cur_img_path = save_image(frame)
             small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            face_locations = face_recognition.face_locations(small_frame)
+            face_encodings = face_recognition.face_encodings(small_frame, face_locations)
 
             score = lookup_known_face(face_encodings[0])
 
-            t1 = time.time()
             is_matched = check_if_matched(score)
 
+            t1 = time.time()
+            print(f'Time to find match: {t1 - t0}')
+
             if is_matched== False:
+                print("Unknown person at Door 2")
                 sendemail.send_email(cur_img_path, "Security Alert - Door 2", "Unidentified Person")
             t2 = time.time()
-
-            print(f'Time to find match: {t1 - t0}')
             print(f'Time to find match and email it: {t2 - t0}')
 
             # os.remove(cur_img_path)
